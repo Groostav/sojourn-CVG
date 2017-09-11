d82a137d09fa370a8533d2c7577719f7e8defc1e
groostav <geoff_groos@msn.com>
2017-09-11 00:02:28 -0700

    added java to kobalt

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,7 @@
+package com.example;
+
+class Main {
+    public static void main(String[] argv) {
+        System.out.println("\n\nHello Java world from Kobalt\n\n");
+    }
+}
---
+++ src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example;
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+    @Test
+    public void f() {
+        System.out.println("Running test");
+    }
+}
