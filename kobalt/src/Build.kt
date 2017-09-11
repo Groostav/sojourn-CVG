e7ff65d5ddff5dcfd5ced46a1308210b1c45d87a
groostav <geoff_groos@msn.com>
2017-09-10 23:56:19 -0700

    ran koblatw init

---
+++ kobalt/src/Build.kt
@@ -0,0 +1,28 @@
+import com.beust.kobalt.*
+import com.beust.kobalt.plugin.packaging.*
+import com.beust.kobalt.plugin.application.*
+import com.beust.kobalt.plugin.kotlin.*
+
+val p = project {
+    name = "sojourn-CVG"
+    group = "com.example"
+    artifactId = name
+    version = "0.1"
+
+    dependencies {
+//        compile("com.beust:jcommander:1.68")
+    }
+
+    dependenciesTest {
+        compile("org.testng:testng:6.11")
+    }
+
+    assemble {
+        jar {
+        }
+    }
+
+    application {
+        mainClass = "com.example.MainKt"
+    }
+}
